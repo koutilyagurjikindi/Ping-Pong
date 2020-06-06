c7e2294bb0bcae1931f2f2b99f5a1685f1c25bbf
koutilyagurjikindi <koutilyagurjikindi@gmail.com>
2020-06-06 13:41:45 +0530

    changes

---
+++ fileTransformer.js
@@ -0,0 +1,8 @@
+// fileTransformer.js
+const path = require('path');
+
+module.exports = {
+  process(filename) {
+    return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+  },
+};
--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,13 @@ module.exports = {
   preset: 'react-native',
   transform: {
     '^.+\\.(js)$': require.resolve('react-native/jest/preprocessor.js'),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
+      '<rootDir>/fileTransformer.js',
+    ),
   },
   setupFiles: [
     '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
     '<rootDir>/src/Game.js',
   ],
   testPathIgnorePatterns: ['<rootDir>/node_modules'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      'jest-transform-stub',
-  },
-  resolver: null,
 };
