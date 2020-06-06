4de2ad660c6b67dccd8f76e2c800cbb9c85f0bda
koutilyagurjikindi <koutilyagurjikindi@gmail.com>
2020-06-06 14:08:08 +0530

    changes

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -2,8 +2,8 @@ global.console = {
   log: jest.fn(), // console.log are ignored in tests
 
   // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
-  error: jest.fn(),
-  warn: jest.fn(),
-  info: jest.fn(),
-  debug: jest.fn(),
+  error: console.error,
+  warn: console.warn,
+  info: console.info,
+  debug: console.debug,
 };
