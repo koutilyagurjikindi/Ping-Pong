90fa7fe2cb2dce552d8aa72c197d264dcf81f70c
koutilyagurjikindi <koutilyagurjikindi@gmail.com>
2020-06-06 13:44:23 +0530

    changes

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
   preset: 'react-native',
   transform: {
     '^.+\\.(js)$': require.resolve('react-native/jest/preprocessor.js'),
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
-      '<rootDir>/fileTransformer.js',
-    ),
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
   },
   setupFiles: [
     '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
