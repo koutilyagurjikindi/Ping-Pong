650ec2ade028adb44233de305d2ceacd2876d813
koutilyagurjikindi <koutilyagurjikindi@gmail.com>
2020-06-06 14:06:25 +0530

    changes

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
   ],
   setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/react-native'],
   moduleNameMapper: {
     '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
       'jest-transform-stub',
