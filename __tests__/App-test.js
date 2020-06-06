0058982b0608d2e690ce1fd32808fc9d65b51e3f
koutilyagurjikindi <koutilyagurjikindi@gmail.com>
2020-06-06 12:55:06 +0530

    chnages

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,10 +4,10 @@
 
 import 'react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
+import jest from 'jest';
 import App from '../App';
 // Note: test renderer must be required after react-native.
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  jest.mock(<App />);
 });
