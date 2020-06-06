caf8cf0b469bfe1fd956ac6150a363d881d0a5da
koutilyagurjikindi <koutilyagurjikindi@gmail.com>
2020-06-06 13:13:53 +0530

    changes

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,8 @@ module.exports = {
     '<rootDir>/src/Game.js',
   ],
   testPathIgnorePatterns: ['<rootDir>/node_modules'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/node_modules/react-native/jest/assetsFileTransformer.js',
+  },
 };
